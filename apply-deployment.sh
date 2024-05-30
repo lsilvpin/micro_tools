81f2ec223d34038c1b34a08a02d90a25186e0165
lsilvpin <lsilvpin@gmail.com>
2024-05-30 13:50:08 -0300

    Ajuste

--- apply-deployment.sh
+++ apply-deployment.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-{
-    echo "Applying deployment..."
-
-    microk8s kubectl apply -f deployment.yaml
-
-    echo "Deployment applied successfully."
-
-    echo "Applying service..."
-
-    microk8s kubectl apply -f service.yaml
-
-    echo "Service applied successfully."
-} || {
-    error_msg=$?
-    echo "An error ocurred"
-    echo $error_msg
-    exit 1
+function throw_error_if_need() {
+    if [ $? -ne 0 ]; then
+        error_msg=$?
+        echo "An error ocurred"
+        echo $error_msg
+        exit 1
+    fi
 }
+
+echo "Applying deployment..."
+
+microk8s kubectl apply -f deployment.yaml
+throw_error_if_need
+
+echo "Deployment applied successfully."
+
+echo "Applying service..."
+
+microk8s kubectl apply -f service.yaml
+throw_error_if_need
+
+echo "Service applied successfully."
