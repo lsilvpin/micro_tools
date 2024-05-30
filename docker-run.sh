f402b5f69f3ad7433abc17a41dcc59827c455c57
lsilvpin <lsilvpin@gmail.com>
2024-05-30 13:56:12 -0300

    Ajustes

--- docker-run.sh
+++ docker-run.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-{
-    echo "Running Docker container..."
-
-    container_name=${1:-"micro_tools_container"}
-    image_name=${2:-"micro_tools_image"}
-    image_tag=${3:-"v1.0.0"}
-    port=${4:-"9000"}
-
-    docker container stop $container_name
-    docker container rm $container_name
-
-    docker run -d \
-        -p $port:8000 \
-        --name $container_name \
-        $image_name:$image_tag \
-        sh -c "sleep 10; uvicorn main.entrypoint.main:app --host 0.0.0.0 --port 8000"
-
-    while [ $(docker container inspect -f '{{.State.Running}}' $container_name) != "true" ]; do
-        echo "Waiting for container to be ready..."
-        sleep 1
-    done
-
-} || {
-    error_msg=$?
-    echo "An error ocurred"
-    echo $error_msg
-    exit 1
+function throw_error_if_need() {
+    if [ $? -ne 0 ]; then
+        echo "An error ocurred"
+        exit 1
+    fi
 }
+
+echo "Running Docker container..."
+
+container_name=${1:-"micro_tools_container"}
+image_name=${2:-"micro_tools_image"}
+image_tag=${3:-"v1.0.0"}
+port=${4:-"9000"}
+
+docker container stop $container_name
+throw_error_if_need
+docker container rm $container_name
+throw_error_if_need
+
+docker run -d \
+    -p $port:8000 \
+    --name $container_name \
+    $image_name:$image_tag \
+    sh -c "sleep 10; uvicorn main.entrypoint.main:app --host 0.0.0.0 --port 8000"
+throw_error_if_need
+
+timeout_sec=15
+counter=0
+while [ $(docker container inspect -f '{{.State.Running}}' $container_name) != "true" ]; do
+    echo "Waiting for container to be ready..."
+    sleep 1
+    counter=$((counter+1))
+    if [ $counter -eq $timeout_sec ]; then
+        echo "Timeout reached"
+        exit 1
+    fi
+done
--- test-is-up.sh
+++ test-is-up.sh
@@ -9,6 +9,7 @@ echo "Host: $host_name"
 echo "Port: $port"
 echo "Protocol: $protocol"
 
+timeout_sec=15
 limit_counter=0
 
 while [ $(curl -s -o /dev/null -w "%{http_code}" $protocol://$host_name:$port/info) != "200" ]; do
@@ -17,7 +18,7 @@ while [ $(curl -s -o /dev/null -w "%{http_code}" $protocol://$host_name:$port/in
     sleep 5
     limit_counter=$((limit_counter+1))
 
-    if [ $limit_counter -eq 10 ]; then
+    if [ $limit_counter -eq $timeout_sec ]; then
         echo "Service is taking too long to start. Exiting..."
         exit 1
     fi
