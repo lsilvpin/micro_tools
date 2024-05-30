ba9d5963205fff828df6b7c84c3ffbe16407cb2a
lsilvpin <lsilvpin@gmail.com>
2024-05-30 11:30:25 -0300

    [Kubernetes] Ajustes nos arquivos yaml

--- apply-deployment.sh
+++ apply-deployment.sh
@@ -5,3 +5,9 @@ echo "Applying deployment..."
 microk8s kubectl apply -f deployment.yaml
 
 echo "Deployment applied successfully."
+
+echo "Applying service..."
+
+microk8s kubectl apply -f service.yaml
+
+echo "Service applied successfully."
